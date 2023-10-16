687f74f05522dac5e6ae4c588b5357db036c20f5
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2023-10-17 00:04:54 +0200

    :tada: commit with the base of the front part in nuxt3

---
+++ pages/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      csjqkdfhjskqcdhsjkqlchdjksql
+  </div>
+</template>
+
+<script lang=ts>
+export default defineComponent({
+    setup() {
+
+    },
+    created() {
+        console.log("created du home")
+    },
+    mounted() {
+
+    },
+    methods: {
+        clicked() {
+            console.log("j'ai cliqué")
+        }
+    }
+
+})
+</script>
+
+<style>
+
+</style>
